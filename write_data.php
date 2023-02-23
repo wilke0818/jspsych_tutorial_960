73c6d39f9703a2b68126341818f0df7c4c724b8a
Mark Saddler <msaddler@mit.edu>
2023-02-23 15:24:16 -0500

    working implementation of data writing on scripts.mit.edu

--- write_data.php
+++ write_data.php
@@ -1,9 +1,23 @@
 <?php
-header('Access-Control-Allow-Origin: *.scripts.mit.edu/*');
-// get the filename and filedata from the POST message
-$post_data = json_decode(file_get_contents('php://input'), true);
-$data = $post_data['filedata'];
-$name = $post_data['filename']; 
-// write the file to disk
-file_put_contents($name, $data);
+// This PHP script receives a POST request and writes its contents to a file.
+// A POST request is a method that JavaScript can send data to a web server.
+// The POST request must include two things for this PHP script to work:
+// - `filename`: specify the full filename of the data being saved
+// - `filedata`: contains the full json/csv data to be saved
+header('Access-Control-Allow-Origin: https://scripts.mit.edu');
+if (isset($_POST['filedata']) == true) {
+    $filedata = $_POST['filedata'];
+} else {
+    echo('write_data.php');
+    exit;
+}
+$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING); // prevent XSS
+if (isset($_POST['filename']) == true) {
+    $filename = $_POST['filename'];
+} else {
+    exit;
+}
+// Write filedata to filename
+file_put_contents($filename, $filedata);
+exit;
 ?>
